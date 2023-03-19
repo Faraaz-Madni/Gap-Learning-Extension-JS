8611c27a8704f255212c84b683d5ac8790733d3a
Faraaz <madnifaraaz@gmail.com>
2023-03-19 07:36:28 +0300

    v0.4.7 migrated to React-Vite-CRXjs, clean up req!

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,58 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+  <>
+    <div className="grid grid-cols-2 max-h-[28px] place-items-center bg-gradient-to-r from-cyan-100 to-blue-200 rounded-xl">
+        <h1 className="text-2xl font-bold bg-gradient-to-r from-cyan-100 to-blue-300 rounded-md px-10 shadow-xl shadow-orange-500/40 ">Opuntia</h1>
+        <h2 className="text-[.7em] px-[3em] place-self-center">A Productivity Extension. </h2>
     </div>
+
+    <div className="grid grid-cols-2 place-items-center ">
+
+        <span className="text-3xl font-bold rounded-md min-h-min max-h-40 px-10 grid grid-rows-1 place-items-center marquee shadow shadow-blue-500/40 hover:shadow-indigo-500/40" >
+          
+        </span>
+        
+        <div className="rounded-md content-center p-5 my-5">
+            <form method="POST" nonce="rAnd0m">
+                <label htmlFor="minutes" className="pb-5 text-[.9em]">How long do you wish to study?</label> 
+                <select id="minutes" name="minutes" defaultValue={90} className="rounded-md font-semibold mt-5 min-w-full px-[10%] py-2">
+                <option value="60" className="text-sm"> 60 Minutes</option>
+                <option value="75" className="text-sm"> 75 Minutes</option>
+                <option value="90" className="text-sm" > 90 Minutes </option>
+                <option value="125" className="text-sm"> 125 Minutes</option>
+                
+                </select>
+                
+                    <input className="items-center rounded-md bg-cyan-100 text-black mt-[10%] min-w-full shadow shadow-blue-500/40 hover:shadow-indigo-500/50 px-[10%] py-2 mb-0 text-sm font-semibold text-gray-900 " type="submit" id="minutes_submit" value="Let's Go!"/>
+
+            </form>
+
+        </div>
+        
+        {/* <!-- <h1> Insert Cactus here! </h1> --> */}
+
+    </div>
+    <div className="grid grid-cols-1 place-items-center">
+        {/* <!-- <h1 className="text-[.5em] m-2 min-w-full"> &nbsp;</h1> --> */}
+        {/* <!-- <h1 className="text-[.5em] m-2 min-w-full">&nbsp;</h1> --> */}
+        <span className="grid grid-cols-2 text-[.7em] mb-5 min-w-full place-items-center">
+            {/* <!-- <h2 className="text-gray-500">by Faraaz Madni </h2> --> */}
+            <h1 className="grid grid-cols-1 place-items-center min-w-full text-gray-700 ">
+                <a className=" " href="https://www.linkedin.com/in/faraaz-madni/"> Cactus </a>
+            </h1>
+            <h1 className="grid grid-cols-1 place-items-center min-w-full text-gray-700 ">
+                <a className=" " href="https://www.linkedin.com/in/faraaz-madni/"> Settings </a>
+            </h1>
+        </span>
+
+    </div>
+  </>
   )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.json'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
